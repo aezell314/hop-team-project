1771146b8fb92746f7180b92a0bd1017ad169a3e
Abigail Ezell <maezell@gmail.com>
2026-04-12 20:27:05 -0500

    adding legend to map

--- hop_team_app/pages/2_Map.py
+++ hop_team_app/pages/2_Map.py
@@ -3,7 +3,8 @@ import pandas as pd
 import folium
 from streamlit_folium import st_folium
 import utils
-
+import branca
+from folium.plugins import BeautifyIcon
 
 st.set_page_config(layout="wide")
 
@@ -15,7 +16,7 @@ with st.sidebar:
     # Create hospital dropdown
     hospital_list = ['All Hospitals'] + sorted(referral_data['owning_entity'].unique())
     selected_org = st.selectbox(
-        'Select a Hospital',
+        'Select a Hospital System',
         options=hospital_list
     )
     
@@ -84,23 +85,38 @@ with st.sidebar:
     delta_arrow="off"
     )
 
-# group the selected community by hospital and calculate total transaction count
-map_df = final_data.groupby(['organization_name','owning_entity','latitude','longitude','color']).agg({'transaction_count':'sum'}).reset_index()
-# add commas to transaction count numbers
-map_df['transaction_count'] = map_df['transaction_count'].apply(lambda x: "{:,}".format(x))
+col1, col2 = st.columns([2, 1])
 
-m = folium.Map(location=[map_df['latitude'].mean(), map_df['longitude'].mean()], zoom_start=8)
-for _, row in map_df.iterrows():
-    iframe = folium.IFrame('<b>Hospital: </b>' + row['organization_name'] + '<br>' + '<b>Part of: </b>' + row['owning_entity'] + '<br>' + '<b>Total Referrals: </b>' + str(row['transaction_count']))
-    popup = folium.Popup(iframe, min_width=300, max_width=400, min_height=100, max_height=100)
-    folium.Marker(location=[row['latitude'], row['longitude']], icon=folium.Icon(color=row['color'], icon='map-marker'), popup=popup).add_to(m)
+with col1:
+    # group the selected community by hospital and calculate total transaction count
+    map_df = final_data.groupby(['organization_name','owning_entity','latitude','longitude','hex_code']).agg({'transaction_count':'sum'}).reset_index()
+    # add commas to transaction count numbers
+    map_df['transaction_count'] = map_df['transaction_count'].apply(lambda x: "{:,}".format(x))
+    
+    m = folium.Map(location=[map_df['latitude'].mean(), map_df['longitude'].mean()], zoom_start=8)
+    for _, row in map_df.iterrows():
+        iframe = folium.IFrame('<b>Hospital: </b>' + row['organization_name'] + '<br>' + '<b>Part of: </b>' + row['owning_entity'] + '<br>' + '<b>Total Referrals: </b>' + str(row['transaction_count']))
+        popup = folium.Popup(iframe, min_width=300, max_width=400, min_height=100, max_height=100)
+        custom_icon = BeautifyIcon(
+            icon='hospital',
+            icon_shape='marker',
+            background_color=row['hex_code']
+        )
+        folium.Marker(location=[row['latitude'], row['longitude']], icon=custom_icon, popup=popup).add_to(m)
+    
+    st_folium(m, width=600, height=400)
 
-st_folium(m, width=900, height=500)
-
-if selected_org != 'All Hospitals' and selected_community != 'All Communities':
-    st.write("Hospitals:")
-    hosp = final_data[['organization_name','org_address']].drop_duplicates(subset=['organization_name','org_address']).rename(columns={'organization_name':'Hospital Name', 'org_address':'Address'})
-    st.dataframe(hosp, use_container_width=True, hide_index=True)
+with col2:
+    legend_df = map_df[['owning_entity','hex_code']].drop_duplicates().sort_values(by='owning_entity').rename(columns={'owning_entity':'Hospital System', 'hex_code':'Color'})
+    
+    # Apply styling to make the color column show the actual color
+    styled_df = legend_df.style.map(
+        lambda x: f'background-color: {x}; color: {x};', 
+        subset=['Color']
+    )
+    
+    st.write("Legend")
+    st.table(styled_df, hide_index=True, width='stretch')
 
 vandy_top_3 = referral_data[(referral_data['hospital_community']==1359) & (referral_data['provider_community']==1359)].groupby('specialization_cleaned')['transaction_count'].sum().sort_values(ascending=False)[0:3].index.to_list()
 
@@ -111,7 +127,12 @@ def highlight_growth_areas(value):
     else:
         return '' 
 
-st.write("Top specializations:")
+if selected_community == 'All Communities' and selected_org == 'All Hospitals':
+    st.write(f"Top PCP specializations for all Middle TN CBSAs:")
+elif selected_community == 'All Communities':
+    st.write(f"Top PCP specializations for referral communities containing {selected_org}:")
+else:
+    st.write(f"Top PCP specializations for referral community #{selected_community}:")
 
 top_spec = final_data.groupby('specialization_cleaned')['transaction_count'].sum().sort_values(ascending=False).head(5).reset_index().rename(columns={'specialization_cleaned':'Specialization','transaction_count':'Total Referrals'})
 top_spec['Total Referrals'] = top_spec['Total Referrals'].apply(lambda x: "{:,}".format(x))
@@ -119,3 +140,12 @@ top_spec['Total Referrals'] = top_spec['Total Referrals'].apply(lambda x: "{:,}"
 st.dataframe(top_spec.style.map(highlight_growth_areas, subset=(top_spec.index[-2:], 'Specialization')), use_container_width=True, hide_index=True)
 
 st.caption("A specialization is highlighted in yellow if it is one of Vanderbilt's strongest specializations (in terms of referral count) and is ranked #4 or #5 in the selected community. These could represent possible areas of growth for Vanderbilt's referral network.")
+
+if selected_community == 'All Communities' and selected_org == 'All Hospitals':
+    st.write(f"Hospitals in all Middle TN CBSAs:")
+elif selected_community == 'All Communities':
+    st.write(f"Hospitals in referral communities containing {selected_org}:")
+else:
+    st.write(f"Hospitals in referral community #{selected_community}:")
+
+st.dataframe(final_data[['organization_name','owning_entity','hospital_community']].sort_values(by=['organization_name', 'hospital_community']).drop_duplicates().rename(columns={'organization_name':'Hospital Name','owning_entity':'Hospital System','hospital_community':'Referral Community'}), use_container_width=True, hide_index=True)
