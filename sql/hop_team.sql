748b5ed815c717c3c914fdaee55e4776f84e1a64
aezell314 <72674922+aezell314@users.noreply.github.com>
2026-05-03 16:09:32 -0500

    Update hop_team.sql

--- sql/hop_team.sql
+++ sql/hop_team.sql
@@ -1,7 +1,6 @@
-
 --  For each provider, identify their primary taxonomy code. In the NPPES data, this is the taxonomy code whose corresponding taxonomy switch column is marked with 'Y'.
 
--- NOTE: there are providers with multiple primary codes. Right now we just select the first one that applies. Improve this later?
+-- NOTE: there are providers with multiple primary codes. Right now we just select the first one that applies. Could be improved later.
 
 with primary_code as (select npi, address_state_name, case
 				when healthcare_primary_taxonomy_switch_1 in ('Y','X') then healthcare_taxonomy_code_1
@@ -27,7 +26,6 @@ left join nucc n
 using(code)
 where p.address_state_name in ('TN', 'Tennessee');
 
-
 with primary_code as (select npi, address_state_name, case
 				when healthcare_primary_taxonomy_switch_1 in ('Y','X') then healthcare_taxonomy_code_1
 				when healthcare_primary_taxonomy_switch_2 in ('Y','X') then healthcare_taxonomy_code_2
@@ -52,7 +50,6 @@ where not code is null
 and address_state_name in ('TN', 'Tennessee');
 --By choosing the first "x" or "y" as the primary specialty, we found 138,120 Tennessee doctors with a primary specialty
 
-
 with indiv_sums as (select 
 COUNT(*) FILTER (WHERE healthcare_primary_taxonomy_switch_1 in ('Y','X')) AS sum1,
 COUNT(*) FILTER (WHERE healthcare_primary_taxonomy_switch_2 in ('Y','X')) AS sum2,
@@ -75,8 +72,8 @@ where address_state_name in ('TN', 'Tennessee')
 select sum1 + sum2 + sum3 + sum4 + sum5 + sum6 + sum7 + sum8 + sum9 + sum10 + sum11 + sum12 + sum13 + sum14 + sum15 as total_yes
 from indiv_sums;
 
---The sum of all "x" and "y" values for all Tennessee doctors is 139,165, which indicates that 1,045 (0.75%) of Tennessee doctors have multiple primary specialties listed in the database. Ex:
-
+--The sum of all primary specialization markers ("X" or "Y") for all doctors in our dataset is 139,165, which indicates that 1,045 (0.75%) of Tennessee doctors have multiple primary specialties listed in the database.
+--Example of a provider that has multiple primary specializations marked in our dataset:
 select npi, healthcare_taxonomy_code_1, healthcare_primary_taxonomy_switch_1, healthcare_taxonomy_code_2, healthcare_primary_taxonomy_switch_2
 from nppes
 where npi = 1649269366;
@@ -152,14 +149,10 @@ on h.to_npi = t.to_npi
 where h.transaction_count >= 50
 and h.average_day_wait < 50;
 
-
-drop materialized view TN_Referrals;
-
-
+-- Exploring the view
 select * from TN_Referrals
-order by providername, organization_name;
-
-select distinct organization_name from TN_Referrals;
+order by providername, organization_name
+limit 10;
 
 -- Identify PCPs who refer patients and the distribution of their referrals across major hospitals.
 select organization_name, sum(patient_count) as total_patients_referred, sum(transaction_count) as total_transactions
@@ -206,5 +199,5 @@ where organization_name = 'VANDERBILT UNIVERSITY MEDICAL CENTER'
 and not specialization is null
 group by specialization
 order by count(providername);
--- Adult Congenital Heart Disease would be an area of growth for Vanderbilt's referral network, with only 1 referring provider in our dataset. 
+-- Adult Congenital Heart Disease would be an area of growth for Vanderbilt's referral network, with only 1 provider referring to Vandy in that specialization in our dataset. 
 
